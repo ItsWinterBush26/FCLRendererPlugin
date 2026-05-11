818d82f4bf571159e5140268c1c5f77d076f900d
ItsWinterBush <154499206+ItsWinterBush26@users.noreply.github.com>
2026-05-12 07:06:11 +0800

    Update application namespace and versioning details

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,49 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
-    namespace = "com.mio.plugin.renderer"
+    namespace = "com.bzlzhh.plugin.ngg"
     compileSdk = 34
 
+    buildFeatures {
+        buildConfig = true
+    }
+
     defaultConfig {
-        applicationId = "com.mio.plugin.renderer"
+        applicationId = "com.bzlzhh.plugin.ngg"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 45
+        versionName = "Release 2026"
+
+        manifestPlaceholders["des"] = "LTW Renderer (OpenGL 3.1+)"
+        manifestPlaceholders["renderer"] = "LTW:libltw.so:libEGL.so"
+        manifestPlaceholders["boatEnv"] = mutableMapOf<String, String>().apply {
+            put("LIBGL_USE_MC_COLOR", "1")
+            put("LIBGL_GL", "31")
+            put("LIBGL_ES", "3")
+            put("LIBGL_NORMALIZE", "1")
+            put("LIBGL_NOERROR", "1")
+        }.run {
+            var env = ""
+            forEach { (key, value) ->
+                env += "$key=$value:"
+            }
+            env.dropLast(1)
+        }
+        manifestPlaceholders["pojavEnv"] =
+            manifestPlaceholders["boatEnv"] as String +
+                    (mutableMapOf<String, String>().apply {
+                        put("POJAV_RENDERER", "opengles3")
+                    }.run {
+                        var env = ":"
+                        forEach { (key, value) ->
+                            env += "$key=$value:"
+                        }
+                        env.dropLast(1)
+                    })
     }
 
     buildTypes {
@@ -20,45 +51,10 @@ android {
             isMinifyEnabled = false
         }
         configureEach {
-            //应用名
-            //app name
-            resValue("string","app_name","LTW)
-            //包名后缀
-            //package name Suffix
-            applicationIdSuffix = ".ltw"
-
-            //渲染器在启动器内显示的名称
-            //The name displayed by the renderer in the launcher
-            manifestPlaceholders["des"] = "LTW (1.17+)"
-            //渲染器的具体定义 格式为 名称:渲染器库名:EGL库名 例如 LTW:libltw.so:libltw.so
-            //The specific definition format of a renderer is ${name}:${renderer library name}:${EGL library name}, for example:   LTW:libltw.so:libltw.so
-            manifestPlaceholders["renderer"] = "LTW:libltw.so:libltw.so"
-
-            manifestPlaceholders["boatEnv"] = mutableMapOf<String,String>().apply {
-                put("LIBGL_ES","3")
-            }.run {
-                var env = ""
-                forEach { (key, value) ->
-                    env += "$key=$value:"
-                }
-                env.dropLast(1)
-            }
-
-            manifestPlaceholders["pojavEnv"] = mutableMapOf<String,String>().apply {
-                put("LIBGL_ES", "3")
-                put("POJAV_RENDERER", "opengles3")
-				put("POJAVEXEC_EGL", "libltw.so")
-				put("LIBGL_EGL", "libltw.so")
-            
-            }.run {
-                var env = ""
-                forEach { (key, value) ->
-                    env += "$key=$value:"
-                }
-                env.dropLast(1)
-            }
+            resValue("string", "app_name", "LTW Renderer)
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -69,5 +65,5 @@ android {
 }
 
 dependencies {
-implementation(project(":LTW"))
+    implementation(project(":LTW"))
 }
